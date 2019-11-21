2710f05e4cd5ef6f317b9c137edf093b8649a32d
Victor Chen <victor.chen2@sky.co.nz>
2019-11-21 18:03:21 +1300

    1: add progress hub 2: remove randomword keywords

--- flutter_version/lib/main.dart
+++ flutter_version/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:english_words/english_words.dart';
 import 'package:http/http.dart' as http;
 import 'dart:developer';
 import 'package:flutter_version/DetailScreen.dart';
+import 'package:progress_hud/progress_hud.dart';
+
+
 
 void main() => runApp(MyApp());
 
@@ -11,24 +14,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Startup Name Generator',
-        home: RandomWords(),
+//        title: 'Startup Name Generator',
+        home: HackerNews(),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
+class HackerNewsState extends State<HackerNews> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  ProgressHUD _progressHUD;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    _progressHUD = new ProgressHUD(
+      backgroundColor: Colors.black12,
+      color: Colors.white,
+      containerColor: Colors.blue,
+      borderRadius: 5.0,
+      loading: false,
+      text: 'Loading...',
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext contect) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hacker News'),
-      ),
-      body: _buildSuggestions(),
+    void dismissProgressHUD() {
+      setState(() {
+        if (_loading) {
+          _progressHUD.state.dismiss();
+        } else {
+          _progressHUD.state.show();
+        }
 
+        _loading = !_loading;
+      });
+    }
+
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('Hacker News'),
+//      ),
+//      body: _buildSuggestions(),
+//
+//    );
+
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('ProgressHUD Demo'),
+        ),
+        body: new Stack(
+          children: <Widget>[
+            new Text(
+                'A clean and lightweight progress HUD for your Flutter app'),
+            _progressHUD,
+//            new Positioned(
+//                child: new FlatButton(
+//                    onPressed: dismissProgressHUD,
+//                    child: new Text(_loading ? "Dismiss" : "Show")),
+//                bottom: 30.0,
+//                right: 10.0)
+          ],
+        )
     );
   }
 
@@ -67,9 +117,7 @@ class RandomWordsState extends State<RandomWords> {
 }
 
 
-class RandomWords extends StatefulWidget {
+class HackerNews extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  HackerNewsState createState() => HackerNewsState();
 }
-
-//https://flutter.dev/docs/get-started/codelab
