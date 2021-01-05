f573a3068b4896bf7d233092e3192e09ae6a70b9
Victor Chen <victorchen276@gmail.com>
2021-01-05 23:32:30 +1300

    add swiftui version

---
+++ iOS_SwiftUI/HackerNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Chen Yue on 5/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
